27d921ca759629619186a2637bfeba13ea91dd2b
Koushal Mandal <koushalmandalbca24@rvu.edu.in>
2026-08-06 10:27:45 +0530

    Add files via upload

---
+++ lab 03/Pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+        <div style={{padding:"60px", minHeight:"70vh"}}>
+            <h1>Welcome to My Website</h1>
+
+            <p>
+                This is the home page.
+            </p>
+        </div>
+    );
+}
+
+export default Home;
